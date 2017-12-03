02a119b91b02100b6ebdee802b7c151b3e436332
Jeff Stafford <jeff.stafford@live.com>
2017-12-03 13:03:32 -0500

    add first slapd test

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -6,10 +6,26 @@ import pytest
 import slapdtest
 
 import ezldap
+from ezldap import LDAP
+
+con = None
+binddn = None
 
 @pytest.fixture(scope='module')
 def slapd():
     instance = slapdtest.SlapdObject()
     instance.start()
+    
+    global binddn
+    binddn = instance.root_dn
+
+    global con
+    config = {
+        'host': instance.ldap_uri,
+        'binddn': instance.root_dn,
+        'binddn_pass': instance.root_pw}
+    con = LDAP(config)
 
 
+def test_bind_success(slapd):
+    assert con.whoami_s() == 'dn:' + binddn
