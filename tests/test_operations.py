591fcafa9000b648fd627efed3f28c09acffe2a3
Jeff Stafford <jeff.stafford@live.com>
2018-03-18 15:18:11 -0400

    test_operations now successfully connects

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -2,8 +2,6 @@
 Test ldap operations on a test instance of slapd.
 '''
 
-import os
-
 import pytest
 import slapdtest
 import ezldap
@@ -11,50 +9,37 @@ import ezldap
 @pytest.fixture(scope='module')
 def slapd():
     instance = slapdtest.SlapdObject()
+    instance.openldap_schema_files = (
+        'core.schema',
+        'nis.schema',
+        'inetorgperson.schema'
+    )
     instance.start()
+    # ldap OUs have not been setup yet in test instance, so we need to create them
+    instance.ldapadd(open('tests/setup.ldif').read())
     
-    global con
-    global config
-    global binddn
-    
-    binddn = instance.root_dn
+    global config    
     config = {
-        'host': instance.ldap_uri,
+        'host': instance.default_ldap_uri,
         'binddn': instance.root_dn,
         'bindpw': instance.root_pw,
         'groupdn': 'ou=Group,' + instance.suffix,
         'peopledn': 'ou=People,' + instance.suffix,
         'homedir': '/home'}
     con = ezldap.auto_bind(config)
-    # ldap OUs have not been setup yet in test instance,
-    # so we need to create them
-    #os.system('ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/cosine.ldif')
-    #os.system('ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/nis.ldif')
-    #os.system('ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/inetorgperson.ldif')
-    #os.system('ldapadd -Y EXTERNAL -H ldapi:/// -f tests/setup.ldif')
-    os.system('ldapadd -x -H {} -D {} -w {} -f /etc/openldap/schema/cosine.ldif'.format(config['host'], config['binddn'], config['bindpw']))
-    os.system('ldapadd -x -H {} -D {} -w {} -f /etc/openldap/schema/nis.ldif'.format(config['host'], config['binddn'], config['bindpw']))
-    os.system('ldapadd -x -H {} -D {} -w {} -f /etc/openldap/schema/inetorgperson.ldif'.format(config['host'], config['binddn'], config['bindpw']))
-    os.system('ldapadd -x -H {} -D {} -w {} -f tests/setup.ldif'.format(config['host'], config['binddn'], config['bindpw']))
-    #con.ldif_add(ezldap.LDIF('tests/setup.ldif'))
+
     assert con.next_uidn() == 10000
     assert con.next_gidn() == 10000
+    return con
 
 
 def test_bind_success(slapd):
-    assert con.whoami_s() == 'dn:' + binddn
+    assert slapd.whoami_s() == 'dn:cn=Manager,dc=slapd-test,dc=python-ldap,dc=org'
 
 
-@pytest.fixture(scope='module')
-def test_no_entries_yet(slapd):
-    assert con.next_uidn() == 10000
-    assert con.next_gidn() == 10000
-
-
-#@pytest.fixture(scope='module')
-def test_add_group(test_no_entries_yet):
-    con.add_group('testgroup', ldif_path='ezldap/templates/ldap-add-group.ldif', **config)
-    assert len(con.get_group('testgroup')) == 1
+def test_add_group(slapd):
+    slapd.add_group('testgroup', ldif_path='ezldap/templates/ldap-add-group.ldif', **config)
+    assert len(slapd.get_group('testgroup')) == 1
 
 
 #def test_add_user(test_add_group):
