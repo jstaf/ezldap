0630739ea09ed2c8672f40296d49970b3ed9979f
Jeff Stafford <jeff.stafford@live.com>
2018-04-14 20:40:40 -0400

    oops

--- tests/test_api.py
+++ tests/test_api.py
@@ -16,17 +16,17 @@ def test_add_group(slapd, config):
     '''
     slapd.add_group('testgroup', ldif_path=prefix + 'ldap-add-group.ldif', **config)
     assert len(slapd.get_group('testgroup')) == 1
-    slapd.add_group('testgroup', gid=50000, ldif_path=prefix + 'ldap-add-group.ldif', **config)
-    assert len(slapd.get_group('testgroup')) == 1
+    slapd.add_group('testgroup2', gid=50000, ldif_path=prefix + 'ldap-add-group.ldif', **config)
+    assert len(slapd.get_group('testgroup2')) == 1
 
 
 def test_add_user(slapd, config):
     '''
     Test adding a user to an existing group.
     '''
-    slapd.add_group('add_user', gid=50001, ldif_path=prefix + 'ldap-add-group.ldif')
+    slapd.add_group('adduser', gid=50001, ldif_path=prefix + 'ldap-add-group.ldif', **config)
     # by groupname
-    slapd.add_user('user1', 'add_user', 'test1234', ldif_path=prefix + 'ldap-add-user.ldif', **config)
+    slapd.add_user('user1', 'adduser', 'test1234', ldif_path=prefix + 'ldap-add-user.ldif', **config)
     assert len(slapd.get_user('user1')) == 1
 
     # by gid
